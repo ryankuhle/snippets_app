a75faeadede0e91505fcaefadf13e4ead4e2ea31
Ryan Kuhle <ryan@ryankuhle.com>
2017-03-11 18:31:57 +0000

    Add parser structure

--- snippets.py
+++ snippets.py
@@ -53,5 +53,16 @@ def main():
 
     arguments = parser.parse_args()
     
+    # Convert parsed arguments from Namespace to dictionary
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+
+    if command == "put":
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {!r}".format(snippet, name))
+    elif command == "get":
+        snippet = get(**arguments)
+        print("Retrieved snippet: {!r}".format(snippet))
+    
 if __name__ == "__main__":
     main()
