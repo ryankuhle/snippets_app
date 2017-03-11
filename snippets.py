5377beb8cf6d10695cfe7b98a04d801c731882ac
Ryan Kuhle <ryan@ryankuhle.com>
2017-03-11 18:06:58 +0000

    Create main function

--- snippets.py
+++ snippets.py
@@ -1,10 +1,17 @@
 import logging
+import argparse
+
+# Argument format when calling script
+# type (put, get), name (name of snippet), snippet (content of snippet)
+# Use positional rather than optional arguments
+# python3 snippets.py put list "A sequence of things - created using []"
 
 # Set the log output file
 logging.basicConfig(filename="snippets.log",level=logging.DEBUG)
 
 def put(name, snippet):
     """
+    put
     Store a snippet with an associated name.
     Returns the name and the snippet.
     """
@@ -16,9 +23,21 @@ def put(name, snippet):
     
 def get(name):
     """
+    get
     Retrieve the snippet with a given name.
     If there is no such snippet, return '404: Snippet Not Found'.
     Returns the snippet.
     """
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     return ""
+    
+def main():
+    """
+    main
+    """
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    arguments = parser.parse_args()
+    
+if __name__ == "__main__":
+    main()
